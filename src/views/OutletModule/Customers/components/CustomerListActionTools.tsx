6663b563e8b19198002928c7dc4bab565dc219f2
Rajinda0621 <rajindasmarasinghe0621@gmail.com>
2025-03-31 09:16:56 +0530

    Customer list

--- src/views/OutletModule/Customers/components/CustomerListActionTools.tsx
+++ src/views/OutletModule/Customers/components/CustomerListActionTools.tsx
@@ -26,7 +26,7 @@ const CustomerListActionTools = () => {
             <Button
                 variant="solid"
                 icon={<TbUserPlus className="text-xl" />}
-                onClick={() => navigate('/concepts/customers/customer-create')}
+                onClick={() => navigate('src\views\MasterSettings\FinalGeography\FinalGeography.tsx')}
             >
                 Add new
             </Button>
