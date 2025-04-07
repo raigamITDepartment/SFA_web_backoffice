ec0013d71cf303d93546b9b4a2de631a1c9c7053
RaVPL96 <raigampasan@gmail.com>
2025-04-07 17:22:41 +0530

    SignIn Api fix

--- src/configs/endpoint.config.ts
+++ src/configs/endpoint.config.ts
@@ -1,7 +1,7 @@
 export const apiPrefix = '/api'
 
 const endpointConfig = {
-    signIn: '/sign-in',
+    signIn: 'http://192.168.8.111:8080/api/v1/auth/login',
     signOut: '/sign-out',
     signUp: '/sign-up',
     forgotPassword: '/forgot-password',
