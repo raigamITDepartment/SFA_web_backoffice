ef83fb5369d0d40a5e5f94aca4c8cda8a38c9d11
RaVPL96 <raigampasan@gmail.com>
2025-06-03 19:53:00 +0530

    new url

--- src/configs/Config.ts
+++ src/configs/Config.ts
@@ -1,2 +1,2 @@
-//export const AuthService_URL = 'https://api-gateway-711667297937.asia-south1.run.app/';
-export const AuthService_URL = 'http://192.168.8.168:8080';
+export const AuthService_URL = 'https://api-gateway-441978242392.us-central1.run.app/';
+//export const AuthService_URL = 'http://192.168.8.168:8080';
